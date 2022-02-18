b742b7e8f0d5c9a3d3852b36fdcdd8953e4c4421
Jury Obuhov <jury2500@gmail.com>
2022-02-18 17:12:59 +0300

    like button

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ const profileAuthor = document.querySelector('.profile__author');
 const profileDescription = document.querySelector('.profile__description');
 const author = formELement.querySelector('.popup__input_type_author');
 const description = formELement.querySelector('.popup__input_type_description');
-
+const popupOpened = 'popup_opened'; // задаем константу модификатора открытия popup.
 
 function openProfilePopup() { // открываем popup
-    author.value = profileAuthor.textContent; // передаем исходное значение в input
-    description.value = profileDescription.textContent; // передаем исходное значение в input
-    editPopup.classList.add('popup_opened');
+    author.value = profileAuthor.textContent; // передаем исходное значение в input.
+    description.value = profileDescription.textContent; // передаем исходное значение в input.
+    editPopup.classList.add(popupOpened);
 }
 
-function closeProfilePopup() { // закрываем popup без сохранения
-    editPopup.classList.remove('popup_opened');
+function closeProfilePopup() { // закрываем popup без сохранения.
+    editPopup.classList.remove(popupOpened);
 }
 
-function formSubmitHandler(evt) { // сохраняем Submit
+function formSubmitHandler(evt) { // сохраняем Submit.
     evt.preventDefault();
     profileAuthor.textContent = author.value;
     profileDescription.textContent = description.value;
 
-    closeProfilePopup(); // после сохранения Submit закрываем popup
+    closeProfilePopup(); // после сохранения Submit закрываем popup.
 }
 
 editPopupButton.addEventListener('click', openProfilePopup);
